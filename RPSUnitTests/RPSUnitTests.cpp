5927732f18d6bbdfbef0d78fbca3465576aa5cde
Collin Livingstone <Clivingstone1043@conestogac.on.ca>
2024-11-01 22:03:22 -0400

    TDD Tests created.

--- RPSUnitTests/RPSUnitTests.cpp
+++ RPSUnitTests/RPSUnitTests.cpp
@@ -11,7 +11,8 @@ namespace RPSUnitTests
 	{
 	public:
 		
-		TEST_METHOD(RockandSissorsTest)
+		//Test1
+		TEST_METHOD(RockBeatsSissorsTest)
 		{
 			//Player choices
 			char playerOneChoice[] = "Rock"; //Player 1
@@ -22,7 +23,104 @@ namespace RPSUnitTests
 
 			//Compare return with result.
 			Assert::AreEqual('1', result);
+		}
 
+		//Test2
+		TEST_METHOD(PaperBeatsRockTest)
+		{
+			//Player choices
+			char playerOneChoice[] = "Rock"; //Player 1
+			char playerTwoChoice[] = "Paper"; //Player2
+
+			//Result
+			char result = RockPaperSissors(playerOneChoice, playerTwoChoice);
+
+			//Compare return with result.
+			Assert::AreEqual('2', result);
+		}
+
+		//Test3
+		TEST_METHOD(RockTiesRockTest)
+		{
+			//Player choices
+			char playerOneChoice[] = "Rock"; //Player 1
+			char playerTwoChoice[] = "Rock"; //Player2
+
+			//Result
+			char result = RockPaperSissors(playerOneChoice, playerTwoChoice);
+
+			//Compare return with result.
+			Assert::AreEqual('t', result);
+		}
+
+		//Test4
+		TEST_METHOD(SissorsBeatsPaperTest)
+		{
+			//Player choices
+			char playerOneChoice[] = "Paper"; //Player 1
+			char playerTwoChoice[] = "Sissors"; //Player2
+
+			//Result
+			char result = RockPaperSissors(playerOneChoice, playerTwoChoice);
+
+			//Compare return with result.
+			Assert::AreEqual('2', result);
+		}
+
+		//Test5
+		TEST_METHOD(PaperTiesPaperTest)
+		{
+			//Player choices
+			char playerOneChoice[] = "Paper"; //Player 1
+			char playerTwoChoice[] = "Paper"; //Player2
+
+			//Result
+			char result = RockPaperSissors(playerOneChoice, playerTwoChoice);
+
+			//Compare return with result.
+			Assert::AreEqual('t', result);
+		}
+
+		//Test6
+		TEST_METHOD(InvalidInputTest)
+		{
+			//Player choices
+			char playerOneChoice[] = "Monkey"; //Player 1
+			char playerTwoChoice[] = "Poptart"; //Player2
+
+			//Result
+			char result = RockPaperSissors(playerOneChoice, playerTwoChoice);
+
+			//Compare return with result.
+			Assert::AreEqual('i', result);
+		}
+
+		//Test7
+		TEST_METHOD(InvalidInputNumbersTest)
+		{
+			//Player choices
+			char playerOneChoice[] = "7"; //Player 1
+			char playerTwoChoice[] = "8"; //Player2
+
+			//Result
+			char result = RockPaperSissors(playerOneChoice, playerTwoChoice);
+
+			//Compare return with result.
+			Assert::AreEqual('i', result);
+		}
+
+		//Test8
+		TEST_METHOD(InvalidNullTest)
+		{
+			//Player choices
+			char playerOneChoice[] = "7"; //Player 1
+			char playerTwoChoice[] = "8"; //Player2
+
+			//Result
+			char result = RockPaperSissors(playerOneChoice, playerTwoChoice);
+
+			//Compare return with result.
+			Assert::AreEqual('i', result);
 		}
 
 	};
